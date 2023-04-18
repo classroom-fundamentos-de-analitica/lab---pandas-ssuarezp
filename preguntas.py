cbf056b0b66d866c4caa4be5979e3d0af6358026
ssuarezp <125924812+ssuarezp@users.noreply.github.com>
2023-04-18 16:39:47 -0500

    V3

--- preguntas.py
+++ preguntas.py
@@ -8,6 +8,7 @@ Utilice los archivos `tbl0.tsv`, `tbl1.tsv` y `tbl2.tsv`, para resolver las preg
 
 """
 import pandas as pd
+import numpy as np
 
 tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
 tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
